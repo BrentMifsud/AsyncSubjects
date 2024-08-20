d403d708ef0dfe484ff7eff3b2b5177451fad173
Brent Mifsud <18543934+BrentMifsud@users.noreply.github.com>
2024-08-20 01:05:00 -0400

    Add AsyncThrowingPublishSubject

---
+++ Sources/AsyncSubject/AsyncPublishSubject/AsyncThrowingPublishSubject.swift
@@ -0,0 +1,107 @@
+//
+//  AsyncThrowingPublishSubject.swift
+//  AsyncSubject
+//
+//  Created by Brent Mifsud on 2024-08-19.
+//
+
+import Foundation
+
+/// A shared `AsyncSequence` that yields value changes to its subscribers or fails
+public struct AsyncThrowingPublishSubject<Element: Sendable>: AsyncSequence, Sendable {
+    public typealias Element = Element
+    public typealias AsyncIterator = AsyncThrowingStream<Element, any Error>.AsyncIterator
+    typealias Continuation = AsyncThrowingStream<Element, any Error>.Continuation
+
+    private let storage: _Storage
+
+    /// A shared `AsyncSequence` that yields value changes to its subscribers
+    public init(){
+        storage = _Storage()
+    }
+
+    init(storage: _Storage) {
+        self.storage = storage
+    }
+
+    /// Yield a new value to subscribers
+    /// - Parameter value: `Element`
+    public func yield(value: Element) async {
+        await storage.yield(value: value)
+    }
+    
+    /// Finish the subject gracefully or with an error
+    /// - Parameter error: error to end the subject with. Send nil to end gracefully
+    public func finish(throwing error: (any Error)? = nil) async {
+        await storage.finish(throwing: error)
+    }
+
+    public func makeAsyncIterator() -> AsyncIterator {
+        return storage.makeAsyncIterator()
+    }
+}
+
+internal extension AsyncThrowingPublishSubject {
+    actor _Storage {
+        private(set) var finished: Bool = false
+        private(set) var failure: (any Error)?
+        private(set) var continuations: [UUID: AsyncThrowingStream<Element, any Error>.Continuation] = [:]
+
+        deinit {
+            for id in continuations.keys {
+                continuations[id]?.finish()
+            }
+        }
+
+        nonisolated func makeAsyncIterator() -> AsyncIterator {
+            let id = UUID()
+            let (stream, continuation) = AsyncThrowingStream<Element, any Error>.makeStream()
+
+            Task {
+                await addContinuation(id: id, continuation: continuation)
+            }
+
+            return stream.makeAsyncIterator()
+        }
+
+        func yield(value: Element) {
+            for id in continuations.keys {
+                continuations[id]?.yield(value)
+            }
+        }
+
+        func finish(throwing error: (any Error)? = nil) {
+            guard !finished else {
+                return
+            }
+
+            finished = true
+            failure = error
+
+            for id in continuations.keys {
+                continuations[id]?.finish(throwing: error)
+                continuations[id] = nil
+            }
+        }
+
+        private func addContinuation(id: UUID, continuation: Continuation) {
+            guard !finished else {
+                continuation.finish(throwing: failure)
+                return
+            }
+
+            continuation.onTermination = { _ in
+                Task { [weak self] in
+                    await self?.removeContinuation(id: id)
+                }
+            }
+
+            continuations[id] = continuation
+        }
+
+        private func removeContinuation(id: UUID) {
+            continuations[id]?.finish()
+            continuations[id] = nil
+        }
+    }
+}
---
+++ Tests/AsyncSubjectTests/AsyncThrowingPublishSubjectTests.swift
@@ -0,0 +1,214 @@
+//
+//  AsyncThrowingPublishSubjectTests.swift
+//  AsyncSubject
+//
+//  Created by Brent Mifsud on 2024-08-20.
+//
+
+import AsyncAlgorithms
+import Foundation
+import Testing
+@testable import AsyncSubject
+
+@Suite("Throwing Publish Subject Tests")
+struct AsyncThrowingPublishSubjectTests {
+    @Test(
+        "(Single subscriber) Test throwing error",
+        arguments: [(
+            valuesToSend: [1, 2, 3, 4 ,5],
+            expectedValues: [1, 2, 3],
+            expectedFailure: NSError(domain: "Test", code: 999)
+        )]
+    )
+    func valuesAreValidAfterThrow(arguments: ([Int], [Int], NSError)) async throws {
+        let (values, expected, failure) = arguments
+        let storage = AsyncThrowingPublishSubject<Int>._Storage()
+        let subject = AsyncThrowingPublishSubject<Int>(storage: storage)
+
+        await storage.validateInitialState()
+
+        await withTaskGroup(of: Void.self) { group in
+            group.addTask {
+                var emittedValues = [Int]()
+
+                do {
+                    for try await value in subject {
+                        emittedValues.append(value)
+                    }
+                } catch {
+                    #expect(emittedValues == expected)
+                    #expect((error as NSError) == failure)
+                    return
+                }
+            }
+
+            group.addTask {
+                await waitFor { await !storage.continuations.isEmpty }
+
+                for value in values {
+                    if value == 4 {
+                        await subject.finish(throwing: failure)
+                        return
+                    } else {
+                        await subject.yield(value: value)
+                    }
+                }
+            }
+
+            await group.waitForAll()
+        }
+
+        await storage.validateEndState()
+    }
+
+    @Test(
+        "(Multiple subscribers) Test throwing error",
+        arguments: [(
+            valuesToSend: [1, 2, 3, 4 ,5],
+            expectedValues: [1, 2, 3],
+            expectedFailure: NSError(domain: "Test", code: 999)
+        )]
+    )
+    func valuesAreValidAfterThrowMultipleSubscribers(arguments: ([Int], [Int], NSError)) async throws {
+        let (values, expected, failure) = arguments
+        let storage = AsyncThrowingPublishSubject<Int>._Storage()
+        let subject = AsyncThrowingPublishSubject<Int>(storage: storage)
+
+        await storage.validateInitialState()
+
+        await withTaskGroup(of: Void.self) { group in
+            let createSubscriber = { @Sendable () async in
+                var emittedValues = [Int]()
+
+                do {
+                    for try await value in subject {
+                        emittedValues.append(value)
+                    }
+                } catch {
+                    #expect(emittedValues == expected)
+                    #expect((error as NSError) == failure)
+                }
+
+                #expect(emittedValues == expected)
+            }
+
+            for _ in 0 ..< 3 {
+                group.addTask {
+                    await createSubscriber()
+                }
+            }
+
+            group.addTask {
+                // wait for all subscribers to connect before sending values
+                await waitFor { await storage.continuations.count == 3 }
+                for value in values {
+                    if value == 4 {
+                        await subject.finish(throwing: failure)
+                        return
+                    } else {
+                        await subject.yield(value: value)
+                    }
+                }
+
+                await subject.finish()
+            }
+
+            await group.waitForAll()
+
+            await storage.validateEndState()
+        }
+    }
+
+    @Test(
+        "(Multiple subscribers) Test staggered subscribers failure",
+        arguments: [(
+            valuesToSend: [1, 2, 3, 4 ,5],
+            expectedValues: [1, 2, 3],
+            failure: NSError(domain: "Test", code: 999)
+        )]
+    )
+    func failedSubscriberDoesNotreceiveCurrentValue(arguments: ([Int], [Int], NSError)) async throws {
+        let (values, expectedValues, failure) = arguments
+        let storage = AsyncThrowingPublishSubject<Int>._Storage()
+        let subject = AsyncThrowingPublishSubject<Int>(storage: storage)
+
+        await storage.validateInitialState()
+
+        await withTaskGroup(of: Void.self) { group in
+            group.addTask {
+                var recievedValues = [Int]()
+                do {
+                    for try await value in subject {
+                        print("Value: \(value)")
+                        recievedValues.append(value)
+                    }
+                } catch {
+                    #expect((error as NSError) == failure)
+                }
+
+                #expect(recievedValues == expectedValues)
+            }
+
+            let trigger = AsyncChannel<Void>()
+
+            group.addTask {
+                // wait for failure before subscribing
+                await waitFor { await storage.failure != nil }
+
+                var recievedValues = [Int]()
+
+                do {
+                    for try await value in subject {
+                        recievedValues.append(value)
+                    }
+                } catch {
+                    #expect((error as NSError) == failure)
+                }
+
+                // The above async for loop should fail immediately because
+                // the subject has already emitted a failure at this point
+                await trigger.send(())
+
+                #expect(recievedValues.isEmpty)
+            }
+
+            group.addTask {
+                // Wait for the first subscriber before sending values
+                await waitFor { await !storage.continuations.isEmpty }
+
+                for value in values {
+                    await subject.yield(value: value)
+                    if value == 3 {
+                        await subject.finish(throwing: failure)
+                        // wait for the second subscriber to attempt subscribing before continuing
+                        await waitFor {
+                            for await _ in trigger {
+                                return true
+                            }
+
+                            return false
+                        }
+                    }
+                }
+            }
+
+            await group.waitForAll()
+        }
+
+        await storage.validateEndState()
+    }
+}
+
+extension AsyncThrowingPublishSubject._Storage {
+    func validateInitialState() {
+        #expect(!finished, "Initial state should not be finished")
+        #expect(failure == nil, "Initial state should not have an error")
+        #expect(continuations.isEmpty, "Initial state should not have continuations")
+    }
+
+    func validateEndState() {
+        #expect(finished, "Final state should be finished")
+        #expect(failure != nil, "Final state should have an error")
+        #expect(continuations.isEmpty, "Final state should not have any continuations")
+    }
+}
